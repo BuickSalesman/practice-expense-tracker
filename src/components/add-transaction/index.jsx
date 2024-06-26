121befdb72201fcde2fd4552e69584dbce68c3ec
BuickSalesman <adam.odonnell.e@gmail.com>
2024-06-26 18:44:34 -0400

    made modal component

---
+++ src/components/add-transaction/index.jsx
@@ -0,0 +1,51 @@
+import {
+  Button,
+  FormControl,
+  FormLabel,
+  Input,
+  Modal,
+  ModalBody,
+  ModalCloseButton,
+  ModalContent,
+  ModalFooter,
+  ModalHeader,
+  ModalOverlay,
+  Radio,
+  RadioGroup,
+} from "@chakra-ui/react";
+
+export default function Transactionform() {
+  return (
+    <Modal>
+      <form>
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader>Add new transaction</ModalHeader>
+          <ModalCloseButton />
+          <ModalBody>
+            <FormControl>
+              <FormLabel>Enter Description</FormLabel>
+              <Input placeholder="Enter transaction description" name="description" type="text" />
+            </FormControl>
+            <FormControl>
+              <FormLabel>Enter Amount</FormLabel>
+              <Input placeholder="Enter transaction amount" name="amount" type="number" />
+            </FormControl>
+            <RadioGroup mt="5">
+              <Radio value="income" colorScheme="blue" name="type">
+                Income
+              </Radio>
+              <Radio value="expense" colorScheme="red" name="type">
+                Expense
+              </Radio>
+            </RadioGroup>
+          </ModalBody>
+          <ModalFooter>
+            <Button mr="4">Cancel</Button>
+            <Button>Add</Button>
+          </ModalFooter>
+        </ModalContent>
+      </form>
+    </Modal>
+  );
+}
